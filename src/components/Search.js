13b703e8b8f81952fff19706d2a6b836f2d0bf8f
Grace Kang <grace@stembolt.com>
2018-08-02 17:04:25 -0700

    Search bar styles

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,7 +3,9 @@ import React from 'react'
 class Search extends React.Component {
 	render() {
 		return (
-			<input type="text" onChange={this.props.onChange} placeholder="Search pokemon" />
+			<div className="Search w-full bg-grey-lightest">
+				<input type="text" onChange={this.props.onChange} className="shadow border rounded py-2 px-3 text-grey-darker" placeholder="Search pokemon" />
+			</div>
 		)
 	}
 }
