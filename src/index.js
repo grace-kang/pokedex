61eeea5bd0560270e1a905984dd60d8d8eaf314c
Grace Kang <grace@stembolt.com>
2018-08-03 16:09:29 -0700

    Keep heroku app awake

--- src/index.js
+++ src/index.js
@@ -14,3 +14,8 @@ render(
 	</Provider>,
 	document.getElementById('root')
 )
+
+var http = require("http");
+setInterval(function() {
+	http.get("http://<your app name>.herokuapp.com");
+}, 300000); // every 5 minutes (300000)
