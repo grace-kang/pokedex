98d81996faae3325704f3000a1e9b23e8555e1e2
grace-kang <gracek316@gmail.com>
2023-09-24 20:13:28 -0700

    Use stylesheet for Pokemon component

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,23 +1,22 @@
-import React from 'react'
+import React from "react";
 
 class Pokemon extends React.Component {
-	render() {
-		const { pokemon } = this.props
-		return (
-			<div className="Pokemon bg-white ml-2 mt-2" onClick={this.props.onClick}>
-				<button className="Pokemon hover:border-red-lighter focus:border-red-lighter border shadow p-2" onClick={this.handleShow}>
-					<img className="Pokemon-sprite"
-						src={"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" + pokemon.id + ".png"}
-						onError={(e)=>{e.target.src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png"}}
-						alt="Pokemon sprite"
-					/>
-					<p className="Pokemon-name text-xs text-grey-darkest uppercase">
-						{pokemon.name}
-					</p>
-				</button>
-			</div>
-		)
-	}
+  render() {
+    const { pokemon } = this.props;
+    const spriteUrl =
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" +
+      pokemon.id +
+      ".png";
+
+    return (
+      <div className="Pokemon" onClick={this.props.onClick}>
+        <button onClick={this.handleShow}>
+          <img src={spriteUrl} alt={pokemon.name} />
+          <p>{pokemon.name}</p>
+        </button>
+      </div>
+    );
+  }
 }
 
-export default Pokemon
+export default Pokemon;
