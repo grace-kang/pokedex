e23b39660766816e2baa79ef47af33225e95cafc
grace-kang <gracek316@gmail.com>
2023-10-07 18:25:47 -0700

    Move Search component CSS to stylesheet

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from "react";
 
 class Search extends React.Component {
-	render() {
-		return (
-			<div className="Search w-full bg-white py-4">
-				<div className="flex justify-center">
-					<input type="text" onChange={this.props.onChange} className="shadow border hover:border-red-lighter rounded py-2 px-3 text-grey-darker focus:border-red-lighter" placeholder="Search pokemon" />
-				</div>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div className="Search">
+        <input
+          type="text"
+          onChange={this.props.onChange}
+          placeholder="Search Pokémon"
+        />
+      </div>
+    );
+  }
 }
 
-export default Search
+export default Search;
