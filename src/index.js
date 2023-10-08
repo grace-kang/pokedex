205f6ec369b5d97ba5d8eb8aaf1e46e2814d5231
grace-kang <gracek316@gmail.com>
2023-10-07 21:47:21 -0700

    Remove interval ping
    This wasn't even pinging an actual URL lol.

--- src/index.js
+++ src/index.js
@@ -14,8 +14,3 @@ render(
 	</Provider>,
 	document.getElementById('root')
 )
-
-var http = require("http");
-setInterval(function() {
-	http.get("http://<your app name>.herokuapp.com");
-}, 300000); // every 5 minutes (300000)
