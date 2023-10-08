578f2933f000655dbb0dff9435aab904a042c676
grace-kang <gracek316@gmail.com>
2023-10-07 21:48:07 -0700

    Refactor `Header` styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,20 @@
-import React from 'react'
-import github from '../images/github.png'
+import React from "react";
+import github from "../images/github.png";
+import "./Header.css";
 
 class Header extends React.Component {
-	render() {
-		return (
-			<div className="Header">
-				<div className="w-full py-10 bg-red-lighter">
-					<a href="https://github.com/grace-kang/pokedex"><img className="absolute pin-r pin-t m-2" src={github} alt="Github" /></a>
-					<div className="flex justify-center items-center">
-						<h1 className="font-black font-sans uppercase text-5xl text-white">Pokédex</h1>
-					</div>
-				</div>
-			</div>
-		)
-	}
+  render() {
+    const githubUrl = "https://github.com/grace-kang/pokedex";
+
+    return (
+      <div className="Header">
+        <a href={githubUrl}>
+          <img src={github} alt="Github" />
+        </a>
+        <span>Pokédex</span>
+      </div>
+    );
+  }
 }
 
-export default Header
+export default Header;
